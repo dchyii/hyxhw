f84eabd1689345c9f3a7c0110051bd317d82c180
Desmond Chong <desmond@90.136.189.118.static.m1net.com.sg>
2023-02-14 13:37:32 +0800

    added logo to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,13 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <div className="w-screen bg-white border-b border-slate-300 px-5 py-2">
-        Navbar
+      <div className="w-screen h-14 overflow-visible bg-white border-b border-slate-300 px-5 py-2 flex">
+        <div className="w-4/5 overflow-visible">
+          <h1 className="font-logo text-5xl">hy x hw </h1>
+        </div>
+        {/* <div className="w-1/5 border border-slate-500">
+          <p className="h-full align-middle text-center text-xl">log in</p>
+        </div> */}
       </div>
     </div>
   );
