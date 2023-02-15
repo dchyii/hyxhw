df241f95562140f04f577985560cb60b8da43535
Desmond Chong <desmond@90.136.189.118.static.m1net.com.sg>
2023-02-15 12:46:32 +0800

    fixed navbar to top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <div className="w-screen h-14 overflow-visible bg-white border-b border-slate-300 px-5 py-2 flex">
+      <div className="w-screen h-14 overflow-visible bg-white border-b border-slate-300 px-5 py-2 flex fixed top-0 left-0 right-0">
         <div className="w-4/5 overflow-visible">
           <h1 className="font-logo text-5xl">hy x hw </h1>
         </div>
