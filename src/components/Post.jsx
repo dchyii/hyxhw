47527f855c246f87147e4fa2e7671e1792b9dac3
Desmond Chong <desmond@90.136.189.118.static.m1net.com.sg>
2023-02-15 12:43:15 +0800

    completed styling and skeleton for post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,8 +2,20 @@ import React from "react";
 
 const Post = () => {
   return (
-    <div className="w-4/5 bg-white mx-auto my-12 border border-slate-300">
-      Post
+    <div className="w-4/5 max-w-lg bg-white mx-auto my-12 border border-slate-300">
+      <img
+        src="https://images.unsplash.com/photo-1574144611937-0df059b5ef3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1064&q=80"
+        className="aspect-auto"
+      />
+      <div className="px-3">
+        <div className="flex mt-3">
+          <h3 className="bg-slate-700 border border-slate-800 rounded-full w-7 h-7 mx-2 text-white text-center">
+            D
+          </h3>
+          <h3>Desmond</h3>
+        </div>
+        <p className=" mb-5">comment comment comment</p>
+      </div>
     </div>
   );
 };
