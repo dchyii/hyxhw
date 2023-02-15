6e57cd1555642f3eda1ae6a7a176df00fe5f5fe6
Desmond Chong <desmond@90.136.189.118.static.m1net.com.sg>
2023-02-15 12:59:00 +0800

    added overflow scroll to main app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Post from "./components/Post";
 
 function App() {
   return (
-    <div className="App bg-slate-100 w-screen h-screen pt-10">
+    <div className="App bg-slate-100 w-screen h-screen pt-10 overflow-scroll">
       <Navbar />
       <Post />
       <Post />
