ae01c40a107ebcf5180775a439f2455363b92ee1
Desmond Chong <desmond@90.136.189.118.static.m1net.com.sg>
2023-02-16 13:10:20 +0800

    added log in button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const Navbar = () => {
   return (
     <div>
-      <div className="w-screen h-14 overflow-visible bg-white opacity-90 border-b border-slate-300 shadow-md px-5 py-2 flex fixed top-0 left-0 right-0">
-        <div className="w-4/5 overflow-visible">
+      <div className="w-screen h-14 overflow-visible bg-white opacity-90 border-b border-slate-300 shadow-md px-5 py-2 flex justify-between fixed top-0 left-0 right-0">
+        <div className="overflow-visible">
           <h1 className="font-logo text-5xl">hy x hw </h1>
         </div>
-        {/* <div className="w-1/5 border border-slate-500">
-          <p className="h-full align-middle text-center text-xl">log in</p>
-        </div> */}
+        <button className="border border-slate-500 px-5 flex items-center content-center shadow-md">
+          <span className="text-xl">log in</span>
+        </button>
       </div>
     </div>
   );
