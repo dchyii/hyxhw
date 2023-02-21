f8840360abd53246dba7add79e89087e02c91a0b
Desmond Chong <desmond@90.136.189.118.static.m1net.com.sg>
2023-02-21 18:18:32 +0800

    extracted post components into Posts component

---
+++ src/components/Posts.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Post from "./Post";
+import PostButton from "./PostButton";
+
+const Posts = () => {
+  return (
+    <div>
+      <Post />
+      <Post />
+      <PostButton />
+    </div>
+  );
+};
+
+export default Posts;
