6d0be3bf1d6d514f32c3efe67e81e26dcf01cd7c
Desmond Chong <desmond@90.136.189.118.static.m1net.com.sg>
2023-02-21 12:50:53 +0800

    initialised firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import Navbar from "./components/Navbar";
 import Post from "./components/Post";
 import PostButton from "./components/PostButton";
+import { db } from "../firebase-config";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const fetchPost = () => {
+      console.log("fetch post data");
+    };
+
+    fetchPost();
+  }, []);
+
   return (
     <div className="App bg-slate-100 w-screen h-screen pt-10 overflow-scroll">
       <Navbar />
