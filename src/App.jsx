98542ec5e14267ea4fa4f205822621c957104a91
Desmond Chong <desmond@90.136.189.118.static.m1net.com.sg>
2023-02-22 13:00:07 +0800

    check if user is signed in on loading

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,29 @@ import Post from "./components/Post";
 import PostButton from "./components/PostButton";
 import { db } from "../firebase-config";
 import { collection, getDocs } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { useState, useEffect } from "react";
 import StartScreen from "./components/StartScreen";
 import Posts from "./components/Posts";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState({});
+
+  const auth = getAuth();
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/firebase.User
+      setUser(user);
+      setIsLoggedIn(true);
+    } else {
+      setUser({});
+      setIsLoggedIn(false);
+    }
+  });
+
   useEffect(() => {
     const fetchPost = () => {
       console.log("fetch post data");
