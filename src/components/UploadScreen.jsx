701dab531d81b9990ccd1dfc2303a5bd5b672d1c
Desmond Chong <desmond@90.136.189.118.static.m1net.com.sg>
2023-02-27 13:41:07 +0800

    created form for upload post

--- src/components/UploadScreen.jsx
+++ src/components/UploadScreen.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 
 const UploadScreen = () => {
+  const handleUpload = (e) => {
+    e.preventDefault();
+    console.log("handle upload");
+  };
   return (
-    <div className="bg-white h-full flex flex-col object-center">
-      UploadScreen
+    <div className="w-full h-full bg-slate-100 flex object-center items-center">
+      <form
+        className="w-4/5 max-w-xl mx-auto p-5 border border-slate-300 shadow-xl rounded-lg flex flex-col items-center"
+        onSubmit={handleUpload}
+      >
+        <input
+          type="file"
+          className="w-full border border-slate-200 rounded-lg px-2 my-2"
+        />
+        <input
+          type="text"
+          placeholder="Please enter your well wishes for the couple ..."
+          className="w-full border border-slate-200 rounded-lg px-2 my-2"
+        />
+        <button
+          type="submit"
+          className="font-logo text-2xl border border-slate-300 enabled:bg-green-300 disabled:bg-slate-200 rounded-lg px-5 py-1 my-3"
+          //   disabled={username.length < 2 ? true : false}
+        >
+          POST!
+        </button>
+      </form>
     </div>
   );
 };
