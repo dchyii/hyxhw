3bfe911ed0f19833903e0fa4dfb4744e5e233d4a
dchyii <hongyichong@gmail.com>
2023-03-08 00:30:56 +0800

    updated title

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = ({ user }) => {
     <div>
       <div className="w-screen h-14 overflow-visible bg-white opacity-90 border-b border-slate-300 shadow-md px-5 py-2 flex justify-between fixed top-0 left-0 right-0">
         <div className="overflow-visible">
-          <h1 className="font-logo text-5xl">hy x hw </h1>
+          <h1 className="font-logo text-5xl">HY and HW </h1>
         </div>
         <div className=" px-5 flex items-center content-center">
           <h3 className="bg-slate-700 border border-slate-800 rounded-full w-7 h-7 mx-2 text-white text-center">
