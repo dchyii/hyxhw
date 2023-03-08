dba85ee77bdc2cc1c3f0de567d6d3c0ee8bc78ac
dchyii <hongyichong@gmail.com>
2023-03-08 09:18:09 +0800

    double click photo to like

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -25,9 +25,20 @@ const Post = ({ postData, currentUser }) => {
     useLike(postData.id, tempArr);
   };
 
+  const handleClick = (e) => {
+    if (e.detail === 2) {
+      handleLike();
+    }
+  };
+
   return (
     <div className="w-4/5 max-w-lg bg-white mx-auto my-8 border-2 border-slate-300 rounded-2xl overflow-hidden shadow-2xl">
-      <img src={postData.url} className="aspect-auto" loading="lazy" />
+      <img
+        src={postData.url}
+        className="aspect-auto"
+        loading="lazy"
+        onClick={handleClick}
+      />
       <div className="px-3">
         <div className="flex mt-3 w-full justify-between">
           <div className="flex">
