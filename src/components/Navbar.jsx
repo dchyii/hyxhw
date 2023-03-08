3965c19fd1b44f11c726c01051b29f2ff4837080
dchyii <hongyichong@gmail.com>
2023-03-08 09:28:19 +0800

    fixed css issue with navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,15 @@ const Navbar = ({ user }) => {
     <div>
       <div className="w-screen h-14 overflow-visible bg-white opacity-90 border-b border-slate-300 shadow-md px-5 py-2 flex justify-between fixed top-0 left-0 right-0">
         <div className="overflow-visible">
-          <h1 className="font-logo text-5xl">HY and HW </h1>
+          <h1 className="font-logo text-3xl md:text-5xl pt-2">HY and HW</h1>
         </div>
-        <div className=" px-5 flex items-center content-center">
+        <div className=" pl-5 flex items-center content-center">
           {user && (
             <h3 className="bg-slate-700 border border-slate-800 rounded-full w-7 h-7 mx-2 text-white text-center">
               {user?.charAt(0).toLowerCase()}
             </h3>
           )}
-          <h3 className="text-xl">{user}</h3>
+          <h3 className="md:text-xl hidden md:block">{user}</h3>
         </div>
       </div>
     </div>
