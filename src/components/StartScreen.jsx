54e206dadc6646a299a449de2c64709f1b6e148b
Desmond Chong <desmond@90.136.189.118.static.m1net.com.sg>
2023-03-23 12:48:22 +0800

    removed challenge from sign up

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -3,39 +3,38 @@ import { getAuth, signInAnonymously, updateProfile } from "firebase/auth";
 
 const StartScreen = ({ fnLogIn }) => {
   const [username, setUsername] = useState("");
-  const [challenge, setChallenge] = useState(0);
 
   const auth = getAuth();
 
   const handleLogIn = (e) => {
     e.preventDefault();
-    if (challenge !== 1703) {
-      alert("The date is incorrect");
-    } else {
-      console.log("logging in:", username);
-      signInAnonymously(auth)
-        .then(() => {
-          // Signed in..
-          const uid = auth.currentUser.uid;
-          updateProfile(auth.currentUser, {
-            displayName: username,
-          })
-            .then(() => {
-              fnLogIn(true);
-            })
-            .catch((error) => {
-              const errorCode = error.code;
-              const errorMessage = error.message;
-              alert(errorMessage);
-            });
+    // if (challenge !== 1703) {
+    //   alert("The date is incorrect");
+    // } else {
+    console.log("logging in:", username);
+    signInAnonymously(auth)
+      .then(() => {
+        // Signed in..
+        const uid = auth.currentUser.uid;
+        updateProfile(auth.currentUser, {
+          displayName: username,
         })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          alert(errorMessage);
-          // ...
-        });
-    }
+          .then(() => {
+            fnLogIn(true);
+          })
+          .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            alert(errorMessage);
+          });
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+        // ...
+      });
+    // }
   };
   return (
     <div className="w-full h-full flex object-center items-center">
@@ -55,25 +54,11 @@ const StartScreen = ({ fnLogIn }) => {
             setUsername(e.target.value);
           }}
         />
-        <p>Please enter our wedding date (in the format ddmm):</p>
-        <input
-          id="challenge"
-          type="number"
-          placeholder="Date and month of our wedding (in the format ddmm)"
-          className="w-full border border-slate-200 rounded-lg px-2 mb-3"
-          value={challenge}
-          onChange={(e) => {
-            setChallenge(e.target.value);
-          }}
-        />
+
         <button
           type="submit"
           className="font-logo text-2xl border border-slate-300 enabled:bg-green-300 disabled:bg-slate-200 rounded-lg px-5 py-1 my-3"
-          disabled={
-            username.length >= 2 && challenge?.toString().length === 4
-              ? false
-              : true
-          }
+          disabled={username.length >= 2 ? false : true}
         >
           Let's Go!
         </button>
